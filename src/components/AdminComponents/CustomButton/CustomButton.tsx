46c8f167351d141b3fdacd52e757541ab443414b
4nh3k <21520579@gm.uit.edu.vn>
2024-04-20 16:23:35 +0700

    Update logo, button styles and props in CustomButton component

--- src/components/AdminComponents/CustomButton/CustomButton.tsx
+++ src/components/AdminComponents/CustomButton/CustomButton.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React from "react";
 
-interface ButtonProps{
-    label: string;
-    textColor: string;
-    btnColor: string;
-    borderColor?: string;
-    imgSrc?: string;
+interface ButtonProps {
+  label: string;
+  textColor: string;
+  btnColor: string;
+  borderColor?: string;
+  imgSrc?: string;
 }
 
-const CustomButton: React.FC<ButtonProps> = ({label, textColor, btnColor, borderColor, imgSrc}) => {
+const CustomButton: React.FC<ButtonProps> = ({
+  label,
+  textColor,
+  btnColor,
+  borderColor,
+  imgSrc,
+}) => {
   return (
-    <button className={`bg-${btnColor} flex w-[8rem] h-10 py-[23px] px-4 justify-center items-center gap-3 rounded-xl border-1 border-solid border-${borderColor}`}>   
-        {imgSrc ? (
-          <img src={imgSrc} width={16} height={16}/>
-        ) : ''}
-        <span className={`text-sm text-${textColor} font-medium leading-[1.125rem]`}>{label}</span>
+    <button
+      className={`bg-${btnColor} flex w-[8rem] h-10 py-[23px] px-4 justify-center items-center gap-3 rounded-xl border-1 border-solid border-${borderColor}`}
+    >
+      {imgSrc ? <img src={imgSrc} width={16} height={16} /> : ""}
+      <span
+        className={`text-sm text-${textColor} font-medium leading-[1.125rem]`}
+      >
+        {label}
+      </span>
     </button>
-  )
-}
+  );
+};
 
-export default CustomButton
+export default CustomButton;
